de6cea70551e37a5284a7f08fd8a1dcf0ba3dea0
vionaosiako <viona.osiako@student.moringaschool.com>
2023-01-19 14:52:51 +0300

    This commit will create Student contractor

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -1,4 +1,38 @@
 package com.example.demo;
 
+import java.time.LocalDate;
+
 public class Student {
+    private  Long id;
+    private  String name;
+    private String email;
+    private LocalDate dob;
+    private Integer age;
+
+    public Student(){
+
+    }
+    public Student(
+                   String name,
+                   String email,
+                   LocalDate dob,
+                   Integer age)
+    {
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public Student(Long id,
+                   String name,
+                   String email,
+                   LocalDate dob,
+                   Integer age){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
 }
